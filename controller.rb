3cb7038bd309fd97ad641eacd5c634a2cc785846
DianaEromosele <diana.ozemebhoya@gmail.com>
2017-04-20 17:51:28 -0400

    Write logic to compare doctors and update/create CSV spreadsheet

--- controller.rb
+++ controller.rb
@@ -25,11 +25,12 @@ class Controller
 			last_name = row[2]
 			gender = row[3]
 			specialties = []
-			specialties << row[4]
-			medical_school = row[6]
-			graduation_year = row[7]
-			active_licenses_in_these_states = row[8]
-			punitive_board_actions_in_these_states = row[9]
+			specialties << row[4].chomp.downcase
+			full_name = row[6]
+			medical_school = row[7]
+			graduation_year = row[8]
+			active_licenses_in_these_states = row[9]
+			punitive_board_actions_in_these_states = row[10]
 
 			states =  {
 		 	'AL' => 'Alabama',
@@ -94,7 +95,7 @@ class Controller
 
 		state = states[row[5]]
 		
-		@doc_delta_doctors << DocDeltaDoctor.new({npi: npi, first_name: first_name, last_name: last_name, gender: gender, specialties: specialties, state: state, medical_school: medical_school, graduation_year: graduation_year, active_licenses_in_these_states: active_licenses_in_these_states, punitive_board_actions_in_these_states: punitive_board_actions_in_these_states})
+		@doc_delta_doctors << DocDeltaDoctor.new({npi: npi, first_name: first_name, last_name: last_name, gender: gender, specialties: specialties, state: state, medical_school: medical_school, full_name: full_name, graduation_year: graduation_year, active_licenses_in_these_states: active_licenses_in_these_states, punitive_board_actions_in_these_states: punitive_board_actions_in_these_states})
 		end
 
 		self.configure_root_path
@@ -139,52 +140,73 @@ class Controller
 	end
 
 	def parse_docInfo_doctor_attributes(docInfoOrg_doctors_XML, doc_delta_doctor)
-		# puts "Doc Delta Doctor: " + doc_delta_doctor.first_name + " " + doc_delta_doctor.last_name
-
 		docInfoOrg_doctors_XML.each do |doctor|
-			first_name = doctor.css("FirstName").text
-			last_name = doctor.css("LastName").text
-			full_name = doctor.css("FullName").text
-			gender = doctor.css("Gender").text
-			specialties = doctor.css("Certifications Certification BoardName").each_with_object([]) do |node, array|
-				array << node.text.delete("*").downcase
-			end
-			reported_states = doctor.css("Locations Location State").each_with_object([]) do |node, array|
-				array << node.text.downcase
-			end
-			medical_school = doctor.css("MedicalSchoolName").text
-			graduation_year = doctor.css("GraduationYear").text
-			active_licenses_in_these_states = doctor.css("Licensures Licensure State").each_with_object([]) do |node, array|
-				array << node.text
-			end
-			punitive_board_actions_in_these_states = doctor.css("BoardActions BoardAction State").each_with_object([]) do |node, array|
-				doctor.css("BoardActions BoardAction StateURL").each do |url|
-					array <<  '<a href="' + url + '">' + node.text + '</a>'
+			first_name = doctor.css("FirstName").text.strip
+			last_name = doctor.css("LastName").text.strip
+			full_name = doctor.css("FullName").text.strip
+			gender = doctor.css("Gender").text.strip
+			specialties = 
+				doctor.css("Certifications Certification BoardName").each_with_object([]) do |node, array|
+					array << node.text.delete("*").strip.downcase
 				end
-			end
+			reported_states = 
+				doctor.css("Locations Location State").each_with_object([]) do |node, array|
+					array << node.text.strip.downcase
+				end
+			medical_school = doctor.css("MedicalSchoolName").text.strip
+			graduation_year = doctor.css("GraduationYear").text.strip
+			active_licenses_in_these_states = 
+				doctor.css("Licensures Licensure State").each_with_object([]) do |node, array|
+					array << node.text.strip
+				end
+			punitive_board_actions_in_these_states = 
+				doctor.css("BoardActions BoardAction State").each_with_object([]) do |node, array|
+					doctor.css("BoardActions BoardAction StateURL").each do |url|
+						array <<  '<a href="' + url + '">' + node.text.strip + '</a>'
+					end
+				end
 			docInfoOrg_doctor = DocInfoOrgDoctor.new({first_name: first_name, last_name: last_name, full_name: full_name, gender: gender, specialties: specialties, reported_states: reported_states, medical_school: medical_school, graduation_year: graduation_year, active_licenses_in_these_states: active_licenses_in_these_states, punitive_board_actions_in_these_states: punitive_board_actions_in_these_states})
-			self.compare_docInfoDoc_to_docDeltaDoc(doc_delta_doctor, docInfoOrg_doctor, )
+			self.compare_docInfoDoc_to_docDeltaDoc(doc_delta_doctor, docInfoOrg_doctor)
 		end	
 
 	end
 
 	def compare_docInfoDoc_to_docDeltaDoc(doc_delta_doctor, docInfoOrg_doctor)
 
-		puts "Before Check"
-		puts doc_delta_doctor.first_name + " " + doc_delta_doctor.last_name
-		puts "Specialties"
-		puts doc_delta_doctor.specialties
+		# puts "------"
+		# puts "START"
+		# puts "------"
+		# puts "Doc Delta Doctor: " + doc_delta_doctor.first_name + " " + doc_delta_doctor.last_name
+		# puts doc_delta_doctor.last_name.length
+		# puts "Gender: #{doc_delta_doctor.gender}"
+		# puts "Specialties: #{doc_delta_doctor.specialties.each { |specialty| puts specialty.downcase }}"
+		
+		# puts "State: #{doc_delta_doctor.state.downcase}"
+
+		# puts "------"
+
+		# puts "DocInfo Doctor: #{docInfoOrg_doctor.first_name} #{docInfoOrg_doctor.last_name}"
+		# puts docInfoOrg_doctor.last_name.length
+		# puts "Gender: #{docInfoOrg_doctor.gender}"
+		# puts "Specialties: #{docInfoOrg_doctor.specialties.each { |specialty| puts specialty.downcase }}"
+		# puts "Reported States: #{docInfoOrg_doctor.reported_states}"
+		# puts "Medical School: #{docInfoOrg_doctor.medical_school}" 
+		# puts "Graduation Year: #{docInfoOrg_doctor.graduation_year}" 
+		# puts "------"
+		# puts "END"
+		
 
 		first_name_check = doc_delta_doctor.first_name.downcase == docInfoOrg_doctor.first_name.downcase
 		last_name_check = doc_delta_doctor.last_name.downcase == docInfoOrg_doctor.last_name.downcase
 		gender_check = doc_delta_doctor.gender.downcase == docInfoOrg_doctor.gender.downcase
-		specialties_check = true
+		specialties_check = false
 			doc_delta_doctor.specialties.each do |specialty|
-				specialties_check = docInfoOrg_doctor.specialties.include?(specialty.downcase) 
+				specialties_check = true if docInfoOrg_doctor.specialties.include?(specialty.downcase) 
 			end
-		state_check = docInfoOrg_doctor.reported_states.include?(doc_delta_doctor.state)   
+		state_check = docInfoOrg_doctor.reported_states.include?(doc_delta_doctor.state.downcase)   
 
 		if first_name_check && last_name_check && gender_check && specialties_check && state_check 
+			doc_delta_doctor.full_name = docInfoOrg_doctor.full_name
 			doc_delta_doctor.specialties = docInfoOrg_doctor.specialties
 			doc_delta_doctor.medical_school = docInfoOrg_doctor.medical_school
 			doc_delta_doctor.graduation_year = docInfoOrg_doctor.graduation_year
@@ -192,19 +214,50 @@ class Controller
 			doc_delta_doctor.punitive_board_actions_in_these_states = docInfoOrg_doctor.punitive_board_actions_in_these_states
 		end 
 
-		puts "First name check: ", first_name_checkc
+		# puts "---------"
+		# puts "Checks"
+		# puts "---------"
 
-		puts "---------"
-		puts "After Check"
 
-		puts doc_delta_doctor.first_name + " " + doc_delta_doctor.last_name
-		puts "Specialties"
-		puts doc_delta_doctor.specialties
-		puts doc_delta_doctor.medical_school
-		puts doc_delta_doctor.graduation_year
-		puts doc_delta_doctor.active_licenses_in_these_states
-		puts doc_delta_doctor.punitive_board_actions_in_these_states
+		# puts "First name check: #{first_name_check}"
+		# puts "Last name check: #{last_name_check}"
+		# puts "Gender check: #{gender_check}"
+		# puts "Specialties check: #{specialties_check}" 
+		# puts "State check: #{state_check}"
 
+		# puts "---------"
+		# puts "After Check"
+		# puts "---------"
+
+
+		# puts "Doc Delta Medical School: #{doc_delta_doctor.medical_school}" 
+		# puts "Doc Delta Graduation Year: #{doc_delta_doctor.graduation_year}"
+		# puts "Doc Delta Active Licenses in these states: #{doc_delta_doctor.active_licenses_in_these_states}" 
+		# puts "Doc Delta Punitive Actions: #{doc_delta_doctor.punitive_board_actions_in_these_states}"
+
+		self.update_CSV(doc_delta_doctor)
+
+	end
+
+	def update_CSV(doc_delta_doctor)
+
+		CSV.open('updated_doc_delta_spreadsheet.csv', 'wb',  :write_headers=> true,
+    :headers => ["NPI","First Name","Last Name", "Gender", "Specialty", "State", "Full Name", "Medical School", "Graduation Year", "Active Licenses", "Actions"]) do |row|
+			# puts row.class
+			row << [doc_delta_doctor.npi, doc_delta_doctor.first_name, doc_delta_doctor.last_name, doc_delta_doctor.gender, doc_delta_doctor.specialties, doc_delta_doctor.state, doc_delta_doctor.full_name, doc_delta_doctor.medical_school, doc_delta_doctor.graduation_year, doc_delta_doctor.active_licenses_in_these_states, doc_delta_doctor.punitive_board_actions_in_these_states]
+			# row[0] << doc_delta_doctor.npi
+			# row[1] << doc_delta_doctor.first_name
+			# row[2] = doc_delta_doctor.last_name
+			# row[3] = doc_delta_doctor.gender
+			# doc_delta_doctor.specialties.each { |specialty| row[4] << specialty }
+			
+			# row[5] = doc_delta_doctor.state
+			# row[6] = doc_delta_doctor.full_name
+			# row[7] = doc_delta_doctor.medical_school
+			# row[8] = doc_delta_doctor.graduation_year
+			# doc_delta_doctor.active_licenses_in_these_states.each { |state| row[9] << state }
+			# doc_delta_doctor.punitive_board_actions_in_these_states.each { |state| row[10] << state }
+		end
 	end
 
 
--- doc_delta_doctor.rb
+++ doc_delta_doctor.rb
@@ -1,11 +1,12 @@
 class DocDeltaDoctor 
-	attr_accessor :specialties, :medical_school, :graduation_year, :active_licenses_in_these_states, :punitive_board_actions_in_these_states
+	attr_accessor :specialties, :medical_school, :graduation_year, :active_licenses_in_these_states, :punitive_board_actions_in_these_states, :full_name
 	attr_reader :npi, :first_name, :last_name, :gender, :state
 	
 	def initialize(args)
 		@npi = args[:npi]
 		@first_name = args[:first_name]
 		@last_name = args[:last_name]
+		@full_name = args[:full_name]
 		@gender = args[:gender]
 		@specialties = args[:specialties] || []
 		@state = args[:state]
